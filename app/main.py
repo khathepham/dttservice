d53d82fc1475dff370db54f157bf58df9f429013
Kha Pham <kha@khathepham.com>
2025-03-23 08:00:45 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ from pydantic import BaseModel
 from camp2025.Registrant import Registrant
 from camp2025 import airtable_database
 from fastapi.middleware.cors import CORSMiddleware
+import logging
+
+lg = logging.getLogger(__name__)
 
 origins = [
     "http://localhost",
@@ -64,38 +67,41 @@ async def register( firstName: Annotated[str, Form()],
                     ):
     print(firstName, lastName, birthdate, gender, nganh, specialNeeds, parentName, parentPhone, parentEmail, childPhoto)
 
-    out_file = childPhoto.filename
+    try:
+        out_file = childPhoto.filename
 
-    if os.path.exists(f"app/camp2025/registrant_images/{out_file}"):
-        out_file_split = os.path.splitext(out_file)
-        out_file = out_file_split[0] + "-%s" + out_file_split[1]
+        if os.path.exists(f"app/camp2025/registrant_images/{out_file}"):
+            out_file_split = os.path.splitext(out_file)
+            out_file = out_file_split[0] + "-%s" + out_file_split[1]
 
-        i = 0
-        while os.path.exists(out_file % i):
-            i += 1
+            i = 0
+            while os.path.exists(out_file % i):
+                i += 1
 
-        out_file = out_file % i
+            out_file = out_file % i
 
-    contents = await childPhoto.read()
+        contents = await childPhoto.read()
 
-    with open(f"app/camp2025/registrant_images/{out_file}", "wb") as file:
-        file.write(contents)
+        with open(f"app/camp2025/registrant_images/{out_file}", "wb") as file:
+            file.write(contents)
 
 
-    registrant = Registrant(
-        first_name=firstName,
-        last_name=lastName,
-        date_of_birth=datetime.datetime.fromisoformat(birthdate),
-        gender=gender.capitalize(),
-        nganh=nganh,
-        special_needs=specialNeeds,
-        guardian_name=parentName,
-        guardian_number=parentPhone,
-        guardian_email=parentEmail,
-        photo_url=f"https://api.doantomathien.org/camp2025/registrant_images/{out_file}",
-    )
+        registrant = Registrant(
+            first_name=firstName,
+            last_name=lastName,
+            date_of_birth=datetime.datetime.fromisoformat(birthdate),
+            gender=gender.capitalize(),
+            nganh=nganh,
+            special_needs=specialNeeds,
+            guardian_name=parentName,
+            guardian_number=parentPhone,
+            guardian_email=parentEmail,
+            photo_url=f"https://api.doantomathien.org/camp2025/registrant_images/{out_file}",
+        )
 
-    airtable_database.create_new_registrant(registrant)
+        airtable_database.create_new_registrant(registrant)
+    except Exception as error:
+        lg.error(error)
 
 
 
